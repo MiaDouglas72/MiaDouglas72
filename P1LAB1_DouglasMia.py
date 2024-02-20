e0f79dbc0ed61b0be7eacda51ba08900a266a497
MiaDouglas72 <160551662+MiaDouglas72@users.noreply.github.com>
2024-02-19 19:13:35 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_DouglasMia.py
@@ -0,0 +1,10 @@
+# Mia Douglas
+# 02/19/2024
+# P1LAB1
+# This program takes a users name as input and prints a welcome message.
+
+name = input("What is your name?")
+print(f"Hey {name}. Welcome to zyBooks!")
+
+
+    
